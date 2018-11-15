2cea53b5f2a9d48f498d836bd3cd5aa45f23fd51
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 11:33:14 +0100

    exo5

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $int= NULL;
+    echo "$int";
+    ?>
+    <?php
+    $int="5";
+    echo "$int";
+    ?>
+  </body>
